2942bc899ee31cd5ed82bcd4cb3bf7d9546e96fd
Juan Diego Molina <134250506+SeniorByteDev@users.noreply.github.com>
2023-08-30 22:05:18 -0500

    fix(repo): update database connection URL

--- meli/infra/repository/connection.py
+++ meli/infra/repository/connection.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "mariadb+mariadbconnector://user:password@127.0.0.1:3306/mydatabase"
+SQLALCHEMY_DATABASE_URL = "mariadb+mariadbconnector://admin:password@192.168.31.200:3306/MELICARS?charset=utf8mb4"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
